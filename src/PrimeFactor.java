1d648372423ddf117ea202f4936efdd5fa921fb6
baseballlover723 <rosspa@rose-hulman.edu>
2015-03-27 22:03:51 -0400

    now passes for generate(1) and generate(2)

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 public class PrimeFactor {
 
 	public static ArrayList<Integer> generate(int k) {
-		return new ArrayList<Integer>();
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if (k == 2) {
+			primes.add(2);
+		}
+		return primes;
 	}
 }
