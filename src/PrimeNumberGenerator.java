a7140604171b705581cffa24fc036bd52af60286
baseballlover723 <rosspa@rose-hulman.edu>
2015-03-27 23:12:20 -0400

    passes test for 3

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -5,6 +5,9 @@ public class PrimeNumberGenerator {
 
 	public static ArrayList<Integer> generate(int k){
 		ArrayList<Integer> primes = new ArrayList<Integer>();
+		if (k > 2) {
+			primes.add(2);
+		}
 		return primes;
 	}
 }
