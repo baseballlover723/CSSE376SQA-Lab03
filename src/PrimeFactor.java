de7b306ee2b615af60c1e3b82421bcd69d148d73
baseballlover723 <rosspa@rose-hulman.edu>
2015-03-27 22:17:49 -0400

    changed code to use a for loop instead of while

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -4,13 +4,11 @@ public class PrimeFactor {
 
 	public static ArrayList<Integer> generate(int k) {
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		int divisor = 2;
-		while (k > 1) {
+		for (int divisor = 2; k > 1; divisor++) {
 			while (k % divisor == 0) {
 				primes.add(divisor);
 				k /= divisor;
 			}
-			divisor++;
 		}
 		if (k > 1) {
 			primes.add(k);
