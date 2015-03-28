4e61bae10fa86a53baeda2bf296cf8cae29a61a3
baseballlover723 <rosspa@rose-hulman.edu>
2015-03-27 23:27:26 -0400

    works for test case 10

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -3,14 +3,22 @@ import java.util.ArrayList;
 
 public class PrimeNumberGenerator {
 
-	public static ArrayList<Integer> generate(int k){
+	public static ArrayList<Integer> generate(int numb){
 		ArrayList<Integer> primes = new ArrayList<Integer>();
-		if (k > 2) {
-			primes.add(2);
-		}
-		if (k > 3) {
-			primes.add(3);
+		for (int k=2; k<numb; k++) {
+			if (isPrime(k)) {
+				primes.add(k);
+			}
 		}
 		return primes;
+	}
+	
+	public static boolean isPrime(int numb) {
+		for (int k=2; k<=Math.sqrt(numb);k++) {
+			if (numb % k == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
