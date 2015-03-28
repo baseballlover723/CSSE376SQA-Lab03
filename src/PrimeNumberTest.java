c183a4d610a254b8ba906883e5c3cfb2c2b395be
baseballlover723 <rosspa@rose-hulman.edu>
2015-03-27 23:08:00 -0400

    test now works as intended

--- src/PrimeNumberTest.java
+++ src/PrimeNumberTest.java
@@ -1,38 +1,41 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
-
+@RunWith(Parameterized.class)
 public class PrimeNumberTest {
 
-	private Integer input;
+	private	Object input;
 	private ArrayList<Integer> expectedResult;
 
-	public void PrimeNumberGeneratorTest(Integer input, ArrayList<Integer> expectedResult) {
+	public PrimeNumberTest(Integer input, ArrayList<Integer> expectedResult) {
 		this.input = input;
 		this.expectedResult = expectedResult;
 	}
 	
 	@Parameters
-	public static Collection primeNumbers() {
+	public static Collection<Object[]> primeNumbers() {
 		return Arrays.asList(new Object[][] {
-				{2, Arrays.asList()},
-				{3, Arrays.asList(new int[] {2})},
-				{4, Arrays.asList(new int[] {2,3})},
-				{5, Arrays.asList(new int[] {2,3})},
-				{10, Arrays.asList(new int[] {2,3,5,7})}
-			});
+				{2, new ArrayList<Integer>()},
+				{3, new ArrayList<Integer>(Arrays.asList(2))},
+				{5, new ArrayList<Integer>(Arrays.asList(2, 3))},
+				{10, new ArrayList<Integer>(Arrays.asList(2, 3, 5, 7))},
+				{20, new ArrayList<Integer>(Arrays.asList(2,3,5,7,11,13,17))},
+		});
 	}
 	
 	@Test
 	public void testPrimeNumberGenerator() {
 		System.out.println("Parameterized Number is : " + this.input);
-		assertEquals(this.expectedResult, PrimeNumberGenerator.generate(this.input));
+		assertEquals(this.expectedResult, PrimeNumberGenerator.generate((Integer) this.input));
 	}
 
 }
